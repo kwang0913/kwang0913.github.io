c51989a1b402db64702a8cb23a15e47fedbe173f
kwang0913 <kailong.wang@outlook.com>
2025-03-31 15:36:17 +0000

    Deploying to gh-pages from @ kwang0913/kwang0913.github.io@e9d1ecc6e811f7799ca3cf64bc2feda6b2c0c70e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -38,16 +38,16 @@ ninja.data = [{
             window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
           
         },
-      },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+      },{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-our-paper-is-accepted-by-sensys-2022",
+          title: 'Our paper is accepted by Sensys 2022',
           description: "",
           section: "News",},{
         id: 'social-email',
